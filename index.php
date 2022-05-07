390e52738b989f8a63a6d31421b1383654c37162
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-05-07 10:18:55 +0900

    add while loop 17

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Loop 17</title>
+</head>
+<body>
+    <?php
+    //Loop
+    //While loop (ini)
+    //DO while loop
+    //For loop
+    //Foreach loop
+    $x = 1;
+    while ($x <= 5){
+        echo "Hi there<br>";
+        $x++;
+
+    }
+    ?>
+</body>
+</html>
